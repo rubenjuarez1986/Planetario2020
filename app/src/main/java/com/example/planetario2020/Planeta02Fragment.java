6a338973524a2333532101b3b07c922838519377
rubenjuarez1986 <rubenjuaeli@gmail.com>
2020-03-13 14:44:19 +0100

    Proyecto Planetario

---
+++ app/src/main/java/com/example/planetario2020/Planeta02Fragment.java
@@ -0,0 +1,28 @@
+package com.example.planetario2020;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Planeta02Fragment extends Fragment {
+
+    public Planeta02Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_planeta02, container, false);
+    }
+}
